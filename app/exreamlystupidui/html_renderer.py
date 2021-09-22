9bb587e7e32a88659641e2923a1f166589026042
Aviad Shalom <Aviad.Shalom@Riskified.com>
2021-09-22 16:30:01 +0200

    undo... will try different approach

--- app/exreamlystupidui/html_renderer.py
+++ app/exreamlystupidui/html_renderer.py
@@ -2,9 +2,15 @@ import django
 from django.conf import settings
 from django.template import Template, Context
 
+
 simplified_recipe_response_template_html = """
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+    </head>
+
+
    <h1 style="color: #5e9ca0; text-align: right;">:המתכון</h1>
    <h2 style="color: #2e6c80; text-align: right;">:מצרכים</h2>
    <ol style="list-style-type: hebrew; direction: rtl;">
@@ -17,6 +23,9 @@ simplified_recipe_response_template_html = """
 """
 
 home_page_html = """"
+<head>
+    <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+</head>
     <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQT2Ql5NWy5w69U3sSQcSrY9VgOEc32M_neFRSd94mSiqJheBuMjfVNXm_K-7SbK3NQUf_AJYlgXrWw/embed?start=false&loop=true&delayms=3000" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     """
 
