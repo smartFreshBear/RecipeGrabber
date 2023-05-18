ddf150ee5d62ce4f08d4443b1c3e9085767e5b98
= <=>
2023-05-18 21:34:02 +0300

    test(server): PLAYG-17 added tester for stored_recipes_manager.py
    testing database is storing, retrieving and deleting info in the proper way.

---
+++ daos/stored_recipes/test/recipes_mocks.py
@@ -0,0 +1,64 @@
+recipes_mocks = [
+        {
+                "ingredients":  [],
+                "instructions": [],
+                "url":          "www.mockito.com",
+                "title":        "mockimo"
+                },
+        {
+                "ingredients":  [
+                        "   לסיר מרק קוסקוס בסגנון מרוקאי",
+                        "   10 שוקיים של עוף אני קוטמת להם את הקצה"
+                        ],
+                "instructions": [
+                        "   המרק",
+                        "  1  מבשלים את הבשר חותכים את הבשר לקוביות מחממים "
+                        ],
+                "title":        "קוסקוס ",
+                "url":
+                    "https://www.yehudit-aviv.co.il/%D7%9E%D7%A8%D7%A7%D7%99"
+                    "%D7%9D/%D7%A7%D7%9/"
+                }, {
+                "ingredients":  [
+                        "Nulla aute minim exercitation excepteur magna magna "
+                        "veniam ad sunt.",
+                        ],
+                "instructions": [
+                        "Cupidatat pariatur nisi incididunt mollit duis ",
+                        "labore non. Deserunt amet consectetur aliqua aute ",
+                        "proident"
+                        ],
+                "title":        "30 Mexican stored_recipes to pair with your "
+                                "margarita",
+                "url":          "https://www.delicious.com.au/recipes/"
+                },
+        {
+                "ingredients":  [],
+                "instructions": [
+                        "Cupidatat pariatur nisi"
+                        ],
+                "title":        "30 Mexican margarita",
+                "url":          "www.google.com"
+                },
+        {
+                "ingredients":  [
+                        "Nulla aute minim exercitation excepteur magna magna "
+                        "veniam ad sunt.",
+                        "quis. Voluptate voluptate sit fugiat aute cupidatat "
+                        "ea anim voluptate sunt eu ullamco anim minim nisi."
+                        ],
+                "instructions": [],
+                "title":        "30 Mexican stored_recipes to pair with your "
+                                "margarita",
+                "url":          "https://www.delicious.com.au"
+                }
+        ]
+
+repeated_urls = [
+        "www.mockito.com",
+        "https://www.yehudit-aviv.co.il/%D7%9E%D7%A8%D7%A7%D7%99%D7%9D/%D7%A7"
+        "%D7%9/",
+        "https://www.delicious.com.au/recipes/",
+        "www.google.com",
+        "https://www.delicious.com.au"
+        ]
