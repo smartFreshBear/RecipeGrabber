7dae1f22e7e3d592b0417d03bb680021df3e7380
= <=>
2023-05-18 21:34:02 +0300

    test(server): PLAYG-17 added tester for stored_recipes_manager.py
    testing database is storing, retrieving and deleting info in the proper way.

--- daos/stored_recipes/test/test_stored_recipes_logic.py
+++ daos/stored_recipes/test/test_stored_recipes_logic.py
@@ -1,56 +1,72 @@
+import atexit
+import os
+import shutil
 import pytest
-
 from flask import Flask
+
 import recipes_mocks
 from daos.database import DataBase
 from daos.stored_recipes import stored_recipes_manager
 
-app = Flask(__name__)
-app.config['URL_TIMEOUT'] = 10
-app.app_context().push()
-with app.app_context():
-	db = DataBase(app)
-	db_instance = db.get_db_instance()
-	db_instance.drop_all()
-	stored_recipe_dao = stored_recipes_manager.RecipeService(
-			db_instance
+
+@pytest.fixture(scope='package')
+def recipes_dao():
+	app = Flask(__name__)
+	with app.app_context():
+		db = DataBase(app)
+		db_instance = db.get_db_instance()
+		recipes_dao = stored_recipes_manager.RecipeService(
+				db_instance
+				)
+		db_instance.create_all()
+		yield recipes_dao
+
+
+def cleanup():
+	current_dir = os.path.dirname(os.path.abspath(__file__))
+	directory_to_delete = os.path.join(
+			current_dir, 'instance'
 			)
-	db_instance.create_all()
+
+	shutil.rmtree(directory_to_delete)
 
 
-def test_insert_logic(self):
+def test_insert_logic(recipes_dao):
 	for recipe in recipes_mocks.recipes_mocks:
 		url = recipe['url']
 		ingred = recipe['ingredients']
 		instruct = recipe['instructions']
-		if not stored_recipe_dao.url_in_db(url):
-			stored_recipe_dao.add_recipe_to_db(response=recipe)
-		stored_recipe = stored_recipe_dao.get_recipe_from_db(
+		if not recipes_dao.url_in_db(url):
+			recipes_dao.add_recipe_to_db(response=recipe)
+		stored_recipe = recipes_dao.get_recipe_from_db(
 				url, ingred, instruct
 				)
 		assert recipe == stored_recipe
 
 
-def test_retrieve_logic(self):
+def test_retrieve_logic(recipes_dao):
 	index = 0
 	while index < len(recipes_mocks.repeated_urls):
 		recipe = recipes_mocks.recipes_mocks[index]
 		url = recipe['url']
 		ingred = recipe['ingredients']
 		instruct = recipe['instructions']
-		if not stored_recipe_dao.url_in_db(url):
-			stored_recipe_dao.add_recipe_to_db(response=recipe)
-		stored_recipe = stored_recipe_dao.get_recipe_from_db(
+		if not recipes_dao.url_in_db(url):
+			recipes_dao.add_recipe_to_db(response=recipe)
+		stored_recipe = recipes_dao.get_recipe_from_db(
 				recipes_mocks.repeated_urls[index], ingred, instruct
 				)
 		assert recipes_mocks.recipes_mocks[index] == stored_recipe
 		index += 1
 
 
-def test_delete_logic(self):
+def test_delete_logic(recipes_dao):
 	for recipe in recipes_mocks.recipes_mocks:
 		url = recipe['url']
-		if not stored_recipe_dao.url_in_db(url):
-			stored_recipe_dao.add_recipe_to_db(response=recipe)
-		stored_recipe_dao.delete_recipe_from_db(url)
-		assert not stored_recipe_dao.url_in_db(url)
+		if not recipes_dao.url_in_db(url):
+			recipes_dao.add_recipe_to_db(response=recipe)
+		recipes_dao.delete_recipe_from_db(url)
+		assert not recipes_dao.url_in_db(url)
+
+
+atexit.register(cleanup)
