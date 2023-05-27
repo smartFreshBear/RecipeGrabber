86199b982dc2590370e9473bad0c6f9499362788
Gev Zoran <gev1zoran@gmail.com>
2023-05-27 23:10:36 +0300

    separate vectorization from main flow created classes to handle vectorization, also added support for english

--- stemming/stemming.py
+++ stemming/stemming.py
@@ -53,13 +53,13 @@ class StemmerHebrew(Stemmer):
             logging.info("had a problem parsing this row {} \n more info: {}".format(row, jsonExc))
 
     def stemmify(self, table):
-        steamed_table = copy.deepcopy(table)
-        only_text_respective = [row[0] for row in steamed_table]
+        stemmed_table = copy.deepcopy(table)
+        only_text_respective = [row[0] for row in stemmed_table]
         self.prepare_stem_mapping(only_text_respective)
 
-        for row_i in range(len(steamed_table)):
-            steamed_table[row_i][0] = self.convert_all_text_to_stem_only(only_text_respective[row_i])
-        return steamed_table
+        for row_i in range(len(stemmed_table)):
+            stemmed_table[row_i][0] = self.convert_all_text_to_stem_only(only_text_respective[row_i])
+        return stemmed_table
 
     def load_from_word_to_stem(self):
         try:
