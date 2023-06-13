5d5ef60ff689d3267fa8c413a46c0c528d3592eb
Israel <israel.bar.dev@gmail.com>
2023-06-13 23:28:07 +0300

    New caching mechanism using Redis server for faster loading of frequently used URLs

--- services/text_extractor_service.py
+++ services/text_extractor_service.py
@@ -1,8 +1,8 @@
 import logging
 import traceback
 from _socket import timeout
-
-from flask import jsonify
+import redis
+from flask import jsonify, json
 
 from algorithms import window_key_word_based_algo
 from apputils import text_prettifer
@@ -11,21 +11,30 @@ from utils import text_extractor
 
 
 class TextExtractorService:
-    def __init__(self, db, executor):
+    def __init__(self, db, executor, caching_manager):
         self.executor = executor
         self.stored_recipes_service = StoredRecipesService(db)
+        self.caching_manager = caching_manager
 
     def find_recipe_in_url(self, form):
         url = form['url']
         instructions = form['instructions'].lower() == "true"
         ingredients = form['ingredients'].lower() == "true"
         try:
+            if self.caching_manager.exists_in_cache(key=url, name="frequently_used_recipes"):
+                return jsonify(json.loads(self.caching_manager.get_from_cache(name="frequently_used_recipes", key=url)))
             if self.stored_recipes_service.url_in_db(url):
-                return jsonify(self.stored_recipes_service.get_recipe_from_db(url, ingredients, instructions))
-            all_text, title = text_extractor.get_all_text_from_url(url=url)
+                recipe = self.stored_recipes_service.get_recipe_from_db(url, ingredients, instructions)
+                self.caching_manager.cache_url(key=url, value=json.dumps(recipe), name="frequently_used_recipes")
+                return jsonify(recipe)
+
+            all_text, title = text_extractor.get_all_text_from_url(url=url, redis=self.caching_manager)
             ingred_paragraph, instr_paragraph = window_key_word_based_algo.extract(ingredients, instructions, all_text)
             response = self.create_json_response(ingred_paragraph, instr_paragraph, title, url)
+
+            self.caching_manager.cache_url(key=url, value=json.dumps(response), name="frequently_used_recipes")
             self.executor.submit(self.stored_recipes_service.add_recipe_to_db, response=response)
+
             return response
         except BlockingIOError as exc:
             return jsonify({"error": f"The requests to {url} has been blocked temporarily "
@@ -38,6 +47,9 @@ class TextExtractorService:
         except TimeoutError as exc:
             logging.error(traceback.print_exc())
             return jsonify({"error": "Threading error"}), 500
+        except redis.RedisError as exc:
+            logging.error(exc)
+            return jsonify({"error": "Redis Caching error"}), 500
 
     def check_if_text_in_recipe(self, form):
         all_text = form['text']
