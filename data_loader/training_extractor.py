67669e61148be4130a8b920af986b18f2347f3f5
yotam7298 <yotamyakov.w@gmail.com>
2023-07-26 23:56:00 +0300

    separate logic for data access and reading

--- data_loader/training_extractor.py
+++ data_loader/training_extractor.py
@@ -13,6 +13,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1NGRUyzImlaUd-UrTkNXSd1JB7OOHnw6e1h4AohRXFK8'
+TEST_INSERT_SPREADSHEET_ID = '1KQ2nFO9oeDLhHmKh9UTvNnhepKhMuTOyKmh7Nnxbj80'
 
 SAMPLE_RANGE_NAME = 'A:C'
 
@@ -21,14 +22,32 @@ training_extractor_logger = create_logger_instance('Training_Extractor')
 """ TODO address + sheets from training set"""
 
 
-def load_all_training_examples(should_print=False, ignore_un_tagged=True):
+def get_values_resource():
     service = get_clinet_to_training_set()
-
-    # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+    values = sheet.values()
+
+    return values
+
+
+def request_to_get_spreadsheet_values():
+    resource = get_values_resource()
+    result = resource.get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                              range=SAMPLE_RANGE_NAME).execute()
     values = result.get('values', [])
+    return values
+
+
+def load_all_training_examples(should_print=False, ignore_un_tagged=True):
+    # service = get_clinet_to_training_set()
+    #
+    # # Call the Sheets API
+    # sheet = service.spreadsheets()
+    # result = sheet_values.get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    #                           range=SAMPLE_RANGE_NAME).execute()
+    # values = result.get('values', [])
+
+    values = request_to_get_spreadsheet_values()
 
     if not values:
         training_extractor_logger.info('No data found.')
