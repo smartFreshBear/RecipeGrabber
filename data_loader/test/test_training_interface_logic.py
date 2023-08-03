3a1129ba420416c6cb9aeb4638dd78e7548ccac2
yotam7298 <yotamyakov.w@gmail.com>
2023-08-03 23:10:43 +0300

    updated test insert for deeper testing

--- data_loader/test/test_training_interface_logic.py
+++ data_loader/test/test_training_interface_logic.py
@@ -1,22 +1,31 @@
 import unittest
 
-from data_loader.training_storer import get_values, insert_values
+from data_loader.training_storer import get_values, insert_values, delete_values
 
 TEST_INSERT_SPREADSHEET_ID = '1KQ2nFO9oeDLhHmKh9UTvNnhepKhMuTOyKmh7Nnxbj80'
 
 
 class TestTrainingInterfaceLogic(unittest.TestCase):
     def test_get_values_data(self):
-        values = get_values()
+        values = get_values(cells_range='A:C', target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
         assert all(isinstance(row, list) for row in values), 'Function returning invalid data'
 
     def test_get_values_not_empty(self):
-        values = get_values()
+        values = get_values(cells_range='A:C', target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
         assert len(values) != 0, 'Function returned an empty list'
 
     def test_insert_values(self):
         cells = [['a', 'b', 'c']]
 
-        response = insert_values(cells, TEST_INSERT_SPREADSHEET_ID)
+        insert_response = insert_values(cells, TEST_INSERT_SPREADSHEET_ID)
 
-        assert response, 'Request returned bad response'
+        assert insert_response is not None, 'Request returned bad response'
+
+        cells_range = insert_response['updates']['updatedRange']
+        values = get_values(cells_range=cells_range, target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
+
+        assert values == cells
+
+        delete_response = delete_values(cells_range, TEST_INSERT_SPREADSHEET_ID)
+
+        assert delete_response['clearedRanges'] == [cells_range]
