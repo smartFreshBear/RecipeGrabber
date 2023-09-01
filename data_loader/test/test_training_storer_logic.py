e7ed53f7ad94354b4d24bb68a0579163d47c7122
Yarin Tabashi <yarintabashi@gmail.com>
2023-09-01 12:11:06 +0300

    Create website_crawler (production+ integration test)
    1. Creating the module 'Website_crawler' that takes text from a recipe website and populates this training example spreadsheet.
    2. Creating the integration test 'test_iterator_and_storer', that insert website content to a spreadsheet (using the website_crawler) and check if it added properly.
    
    * I had duplicated the token to the relevant folder.

--- data_loader/test/test_training_storer_logic.py
+++ data_loader/test/test_training_storer_logic.py
@@ -2,30 +2,30 @@ import unittest
 
 from data_loader.training_storer import get_values, insert_values, delete_values
 
-TEST_INSERT_SPREADSHEET_ID = '1KQ2nFO9oeDLhHmKh9UTvNnhepKhMuTOyKmh7Nnxbj80'
+TEST_SPREADSHEET_ID = '1KQ2nFO9oeDLhHmKh9UTvNnhepKhMuTOyKmh7Nnxbj80'
 
 
 class TestTrainingInterfaceLogic(unittest.TestCase):
     def test_get_values_data(self):
-        values = get_values(cells_range='A:C', target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
+        values = get_values(cells_range='A:C', target_spreadsheet=TEST_SPREADSHEET_ID)
         assert all(isinstance(row, list) for row in values), 'Function returning invalid data'
 
     def test_get_values_not_empty(self):
-        values = get_values(cells_range='A:C', target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
+        values = get_values(cells_range='A:C', target_spreadsheet=TEST_SPREADSHEET_ID)
         assert len(values) != 0, 'Function returned an empty list'
 
     def test_insert_values(self):
         cells = [['a', 'b', 'c']]
 
-        insert_response = insert_values(cells, TEST_INSERT_SPREADSHEET_ID)
+        insert_response = insert_values(cells, TEST_SPREADSHEET_ID)
 
         assert insert_response is not None, 'Request returned bad response'
 
         cells_range = insert_response['updates']['updatedRange']
-        values = get_values(cells_range=cells_range, target_spreadsheet=TEST_INSERT_SPREADSHEET_ID)
+        values = get_values(cells_range=cells_range, target_spreadsheet=TEST_SPREADSHEET_ID)
 
         assert values == cells
 
-        delete_response = delete_values(cells_range, TEST_INSERT_SPREADSHEET_ID)
+        delete_response = delete_values(cells_range, TEST_SPREADSHEET_ID)
 
         assert delete_response['clearedRanges'] == [cells_range]
