cb68e192430107e78e9f805ad015cb7be6db1ab4
Yarin Tabashi <yarintabashi@gmail.com>
2023-09-01 11:57:45 +0300

    Fix bugs in website iterator (production+test)
    Modify production:
    * Added the option to choose paragraph size.
    * Fixing bugs in 'next' method.
    
    Modify test:
    * Added caching manager (it was required)
    * Ensure that 'next' raises a 'StopIteration' when there are no more items to yield.

--- training/website_iterator.py
+++ training/website_iterator.py
@@ -1,24 +1,29 @@
 from utils import text_extractor
-SIZE_OF_TRAINING_EXAMPLE = 5
+DEFAULT_PARA_SIZE = 5
+
 
 class WebsiteIterator:
-    def __init__(self, url):
+    def __init__(self, url, caching_manager, paragraph_size):
         self.url = url
-        self.content = text_extractor.get_all_text_from_url(url)
+        self.content = (text_extractor.get_all_text_from_url(url, caching_manager=caching_manager))[0]
         self.index = 0
         self.content_length = len(self.content)
+        self.paragraph_size = paragraph_size
+
 
     def next(self):
         lines = self.content.splitlines()
-        end_index = self.index + SIZE_OF_TRAINING_EXAMPLE
-        if end_index >= self.content_length:
-            end_index = self.content_length - self.index
+        if self.index == 0:
+            self.content_length = len(lines)
+        end_index = min(self.content_length, self.index+self.paragraph_size)
         if self.index >= self.content_length:
             raise StopIteration
         else:
             result = lines[self.index:end_index]
-            self.index += SIZE_OF_TRAINING_EXAMPLE
+            if len(result) == 0:
+                raise StopIteration
+            self.index += self.paragraph_size
             return result
 
-    def get_iterator_for_website(url):
-        return WebsiteIterator(url)
+    def get_iterator_for_website(url, caching_manager, paragraph_size=DEFAULT_PARA_SIZE):
+        return WebsiteIterator(url, caching_manager, paragraph_size)
